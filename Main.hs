1da66f707203e9091658e2ee9301b3cea2e8f8e5
robin <palotai.robin@gmail.com>
2012-10-10 11:58:23 +0200

    added ability to make two units fight until death

--- Main.hs
+++ Main.hs
@@ -12,24 +12,37 @@ damage u @ Unit { remainingHp = HP hp } dmg =
        cond (hp > dmg) $ u { remainingHp = HP $ hp - dmg }
 
 shoot :: Unit -> Unit -> Maybe Unit
-shoot a b = damage b (runFp $ firepower a) 
+shoot x y = damage y (runFp $ firepower x) 
 
 --
--- may Hoogle for it
+-- fight has a bias, the left unit gets to shoot first
 --
+fight :: Unit -> Unit -> Either Unit Unit
+fight x y = 
+  let damagedY = x `shoot` y
+  in maybe (Left x) (\y' -> swap $ fight y' x) damagedY
+  
+
+--
+-- Helpers
+--
+swap :: Either a b -> Either b a
+swap (Left x)  = Right x 
+swap (Right x) = Left x
+
 cond :: Bool -> a -> Maybe a
 cond True a = Just a
 cond False a = Nothing
 
---
--- Binds repeatedly
---
 repeatBind :: (Monad m) => Int -> m a -> (a -> m a) -> m a
 repeatBind n ma f = foldl (>>=) ma (replicate n f)
 
+--
+-- Main
+--
 main :: IO ()
 main = do
      let unit1 = Unit (HP 100) (Firepower  5)
      let unit2 = Unit (HP  50) (Firepower 12)
-     let unit2' = repeatBind 5 (Just unit2) (shoot unit1) 
-     putStrLn $ show (remainingHp unit2) ++ " --> " ++ show (fmap remainingHp unit2')
+     let winner = unit1 `fight` unit2
+     putStrLn $ show unit1 ++ " vs " ++ show unit2 ++ "  --> " ++ show winner 
