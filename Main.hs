15a7f834b558bdd9d011b4d587796d53a5100f0c
robin <palotai.robin@gmail.com>
2012-10-24 23:43:02 +0200

    using difference list for logging

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Control.Monad.Writer
 import Control.Monad.Identity
 import Control.Monad.Trans.Error
 import Control.Monad.Trans.Maybe
+import Data.DList hiding (replicate)
 
 data HP = HP Int
      deriving Show
@@ -18,7 +19,7 @@ instance Show Unit where
     in
       "Unit '" ++ name u ++ "' [HP:" ++ show hp ++ " FP:" ++ show fp ++ "]"
 
-type Logged = Writer [String]
+type Logged = Writer (DList String)
 
 --
 -- Battling
@@ -50,7 +51,7 @@ maybeT :: (Functor m) => b -> (a -> b) -> MaybeT m a -> m b
 maybeT fallback f ma = fmap (maybe fallback f) $ runMaybeT ma 
 
 logged :: a -> String -> Logged a 
-logged x s = writer (x, [s]) 
+logged x s = writer (x, fromList [s]) 
 
 swap :: Either a b -> Either b a
 swap (Left x)  = Right x 
@@ -66,6 +67,8 @@ cond False a = Nothing
 repeatBind :: (Monad m) => Int -> m a -> (a -> m a) -> m a
 repeatBind n ma f = foldl (>>=) ma (replicate n f)
 
+mapSnd f (x, y) = (x, f y)
+
 --
 -- Main
 --
@@ -74,5 +77,5 @@ main = do
      let unit1 = Unit "Fizz" (HP 100) (Firepower  5)
      let unit2 = Unit "Buzz" (HP  50) (Firepower 12)
      let winner = unit1 `fight` unit2
-     let winnerText = show $ runIdentity $ runWriterT $ runErrorT $ winner
+     let winnerText = show $ mapSnd toList $ runIdentity $ runWriterT $ runErrorT $ winner
      putStrLn $ show unit1 ++ " vs " ++ show unit2 ++ "  --> " ++ winnerText
